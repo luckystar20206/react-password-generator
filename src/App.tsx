2a94f94231f900ef56a11217f10ff08e67773a58
luckystar20206 <luckystar20206@gmail.com>
2024-03-30 13:58:40 +0530

    add copy password to clipboard

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useState } from 'react'
+import { useCallback, useEffect, useRef, useState } from 'react'
 import './App.css'
 
 function App() {
@@ -40,14 +40,21 @@ function App() {
     passwordGenerator()
   }, [length, uppercaseAllowed, lowercaseAllowed, numbersAllowed, specialCharsAllowed])
 
+  const passwordReference = useRef(null);
+
+  const copyPasswordToClipboard = useCallback(() => {
+    passwordReference.current?.select();
+    window.navigator.clipboard.writeText(password);
+  }, [password])
+
   return (
     <div className='w-6/12 mx-auto mt-12 py-8 border border-1 border-slate-500 bg-violet-200 rounded-lg'>
       <h1 className='text-center text-3xl mb-4 font-bold'>Random Password Generator</h1>
       <p className='text-center mb-8'>Create strong and secure passwords to keep your account safe online.</p>
       <div className='text-center'>
-        <input type="text" placeholder='password' className='px-6 py-2 rounded-full mr-6' value={password} readOnly/>
+        <input type="text" placeholder='password' className='px-6 py-2 rounded-full mr-6 outline-none border-none select-none' value={password} readOnly ref={passwordReference}/>
         <i className="fa-solid fa-rotate mr-6 cursor-pointer" onClick={() => passwordGenerator()}></i>
-        <button className='bg-blue-600 text-white px-3 py-2 rounded-full w-24 font-bold'>Copy</button>
+        <button id="copy-button" className='copy-button bg-blue-600 text-white px-3 py-2 rounded-full w-24 font-bold select-none' onClick={() => copyPasswordToClipboard()}>Copy</button>
       </div>
       <div className='flex justify-center gap-10 my-8'>
         <p className='w-24'>Password length: <span className='font-bold'>{length}</span></p>
